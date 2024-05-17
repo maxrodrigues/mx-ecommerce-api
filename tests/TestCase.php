89d278c53c0689f3ad8a28489fc84de2b45db7b5
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-05-17 17:31:42 -0300

    feat: add request function, config db

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function request($method, $uri, $data = [], $headers = [])
+    {
+        return $this->json($method, $uri, $data, $headers);
+    }
 }
