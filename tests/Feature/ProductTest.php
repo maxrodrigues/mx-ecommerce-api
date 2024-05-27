7c8ed715aa69d31817a704cd6f13895ceee08607
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-05-27 15:37:32 -0300

    feat: products todos

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,16 @@
+<?php
+
+todo ('returns all registered products');
+todo ('should return the details of a product');
+todo ('should be must search for products by name');
+todo ('should be must search for products by ean');
+todo ('should be return products when search by category');
+todo ('return success when receiving all the attributes necessary to register the product');
+todo ('return error when required attributes are not sent');
+todo ('return error when trying to register a product already registered exists');
+todo ('return success and product detail when updated successfully');
+todo ('should return an error when the product is not updated');
+todo ('should return success when the product is deleted successfully');
+todo ('should return an error when the product is not deleted');
+todo ('should return an error when the product does not exist');
+todo ('should return an error when the product is not found');
