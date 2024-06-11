3feed86cc7c6d91f3ab96c4d19e12d6783d9ca77
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-06-11 17:47:01 -0300

    feat: error when required attributes is missing

--- app/Http/Controllers/Tags/TagController.php
+++ app/Http/Controllers/Tags/TagController.php
@@ -7,6 +7,7 @@ use App\Models\Tag;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class TagController extends Controller
@@ -14,9 +15,20 @@ class TagController extends Controller
     public function store(Request $request): JsonResponse
     {
         try {
-            Tag::create([
-                'name' => $request->name,
+            $data = Validator::make($request->all(), [
+                'name' => 'required|min:3|max:255|unique:tags',
             ]);
+
+            if ($data->fails()) {
+                return new JsonResponse([
+                    'data' => [
+                        'message' => 'The given data was invalid.',
+                        'errors' => $data->errors(),
+                    ],
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            Tag::create($data->validated());
 
             return new JsonResponse([
                 'data' => [
--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -19,3 +19,24 @@ it ('return success when tag is created successfully', function () {
     $response->assertStatus(Response::HTTP_CREATED);
     $this->assertDatabaseCount('tags', 1);
 });
+
+it ('return error when required attributes is missing', function () {
+    $user = createUserAdmin();
+    $login = $this->request(method: 'POST', uri: 'api/admin/login', data: [
+        'email' => $user['email'],
+        'password' => 'password',
+    ]);
+    $token = json_decode($login->content(), true)['data']['token'];
+    $response = $this->request(method: 'POST', uri: '/api/tags',  headers: [
+        'Authorization' => 'Bearer ' . $token,
+    ]);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJson([
+            'data' => [
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'name' => ['The name field is required.'],
+                ],
+            ],
+        ]);
+});
