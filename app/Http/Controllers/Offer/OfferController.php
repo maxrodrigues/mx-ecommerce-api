ffb8e06443caa825699670e3d91280e88df95f3e
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-06-20 15:00:22 -0300

    feat: return error when required parameter is missing

--- app/Http/Controllers/Offer/OfferController.php
+++ app/Http/Controllers/Offer/OfferController.php
@@ -7,6 +7,7 @@ use App\Models\Offer;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class OfferController extends Controller
@@ -14,6 +15,23 @@ class OfferController extends Controller
     public function store(Request $request): JsonResponse
     {
         try {
+            $data = Validator::make($request->all(), [
+                'name' => 'required|string|unique:offers,name',
+                'code' => 'required|string|min:3|max:20|unique:offers,code',
+                'discount' => 'required|numeric',
+                'start_at' => 'required',
+                'finish_at' => 'required',
+            ]);
+
+            if ($data->fails()) {
+                return new JsonResponse([
+                    'data' => [
+                        'message' => 'The given data was invalid.',
+                        'errors' => $data->errors()
+                    ]
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
             Offer::create($request->all());
             return new JsonResponse([
                 'data' => [
--- tests/Feature/OffersTest.php
+++ tests/Feature/OffersTest.php
@@ -57,3 +57,38 @@ it ('should be return success when store an offer', function () {
         ]);
     $this->assertDatabaseCount('offers', 1);
 });
+
+it ('should be return error when required parameter is missing', function () {
+    $adminUser = createUserAdmin();
+    $adminLogin = $this->request(method: 'POST', uri: 'api/admin/login', data: [
+        'email' => $adminUser['email'],
+        'password' => 'password',
+    ]);
+    $adminToken = json_decode($adminLogin->content(), true)['data']['token'];
+    $adminRequest = $this->request(method: 'POST', uri: 'api/offers', headers: [
+        'Authorization' => 'Bearer ' . $adminToken
+    ]);
+    $adminRequest->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJson([
+            'data' => [
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'name' => [
+                        'The name field is required.'
+                    ],
+                    'code' => [
+                        'The code field is required.'
+                    ],
+                    'discount' => [
+                        'The discount field is required.'
+                    ],
+                    'start_at' => [
+                        'The start at field is required.'
+                    ],
+                    'finish_at' => [
+                        'The finish at field is required.'
+                    ]
+                ]
+            ],
+        ]);
+});
